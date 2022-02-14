10fdb7b6e68210e04b09798a018a6bb84de8db69
Allankiyochi <allankiyochi@gmail.com>
2022-02-13 22:57:16 -0300

    Desafio final Dio

---
+++ frontend/src/components/store/reducers/product.js
@@ -0,0 +1,159 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "oficiais",
+      fk_idcategorys: 1,
+      name_product: "Camisa 1",
+      price: 300,
+      image: "./images/produtos/camisa 1.jpg",
+      id_categorys: 1,
+      name_categorys: "Camisa de Jogo",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "oficiais",
+      fk_idcategorys: 1,
+      name_product: "Camisa 2",
+      price: 300,
+      image: "./images/produtos/camisa 2.jpg",
+      id_categorys: 1,
+      name_categorys: "Camisa de Jogo",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "oficiais",
+      fk_idcategorys: 1,
+      name_product: "Camisa 3",
+      price: 300,
+      image: "./images/produtos/camisa 3.jpg",
+      id_categorys: 1,
+      name_categorys: "Camisa de Jogo",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "goleiro",
+      fk_idcategorys: 2,
+      name_product: "Goleiro 1",
+      price: 399.99,
+      image: "./images/produtos/goleiro 1.jpg",
+      id_categorys: 2,
+      name_categorys: "Goleiro de Jogo",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "goleiro",
+      fk_idcategorys: 2,
+      name_product: "Goleiro 2",
+      price: 399.99,
+      image: "./images/produtos/goleiro 2.jpg",
+      id_categorys: 2,
+      name_categorys: "Goleiro de Jogo",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "goleiro",
+      fk_idcategorys: 2,
+      name_product: "Goleiro 3",
+      price: 399.99,
+      image: "./images/produtos/goleiro 4.jpg",
+      id_categorys: 2,
+      name_categorys: "Goleiro de Jogo",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "historica",
+      fk_idcategorys: 3,
+      name_product: "Barbosa 100 anos",
+      price: 399.99,
+      image: "./images/produtos/barbosa 100.jpg",
+      id_categorys: 3,
+      name_categorys: "Camisas históricas",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "shorts",
+      fk_idcategorys: 4,
+      name_product: "Shorts 1",
+      price: 150,
+      image: "./images/produtos/shorts 1.jpg",
+      id_categorys: 4,
+      name_categorys: "Shorts de jogo",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "shorts",
+      fk_idcategorys: 4,
+      name_product: "Shorts 2",
+      price: 150,
+      image: "./images/produtos/shorts 2.jpg",
+      id_categorys: 4,
+      name_categorys: "Shorts de jogo",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "shorts",
+      fk_idcategorys: 4,
+      name_product: "Shorts 3",
+      price: 150,
+      image: "./images/produtos/shorts 3.jpg",
+      id_categorys: 4,
+      name_categorys: "Shorts de jogo",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "outras",
+      fk_idcategorys: 5,
+      name_product: "Treino",
+      price: 180,
+      image: "./images/produtos/treino.jpg",
+      id_categorys: 5,
+      name_categorys: "Outras",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "outras",
+      fk_idcategorys: 5,
+      name_product: "Comissão Técnica",
+      price: 189.99,
+      image: "./images/produtos/tecnico.jpg",
+      id_categorys: 5,
+      name_categorys: "Outras",
+      selected: false
+    },
+    {
+      id_product: 13,
+      category: "calca",
+      fk_idcategorys: 5,
+      name_product: "Shorts Goleiro 1",
+      price: 189.99,
+      image: "./images/produtos/shorts gol 1.jpg",
+      id_categorys: 5,
+      name_categorys: "Shorts Goleiro",
+      selected: false
+    },
+    {
+      id_product: 14,
+      category: "calca",
+      fk_idcategorys: 5,
+      name_product: "Shorts Goleiro 2",
+      price: 189.99,
+      image: "./images/produtos/shorts gol 2.jpg",
+      id_categorys: 5,
+      name_categorys: "Shorts Goleiro",
+      selected: false
+    },
+
+];
+
+export default productsList;
